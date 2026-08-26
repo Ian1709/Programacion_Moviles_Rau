f244200538119913d08ec4f31d600eae3de60c59
Ian Rau <ian.rau.1721@gmail.com>
2026-08-26 03:00:53 -0500

    Se agrega data class y lista inicial de productos.

--- Semana02/Semana02Console/src/main/java/com/rau/semana02console/Carrito.kt
+++ Semana02/Semana02Console/src/main/java/com/rau/semana02console/Carrito.kt
@@ -1,7 +1,26 @@
 package com.rau.semana02console
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    val cantidad: Int
+)
+
 fun main() {
     println("=========================================")
     println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
     println("=========================================")
+
+    val nombreCliente = "Ian Rau" // String (inferido)
+    val carrito = mutableListOf<Producto>() // lista vacía de productos
+    println("Cliente: $nombreCliente\n")
+
+    carrito.add(Producto("Laptop HP", 2500.0, 1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Audifonos Sony", 120.0, 1))
+    carrito.add(Producto("USB Kingston 64GB", 25.0, 3))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
 }
