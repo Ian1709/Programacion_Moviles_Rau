aa37e12b1b75a04ff5b19f017ff12660059ececb
Ian1709 <ian.rau.1721@gmail.com>
2026-09-16 03:28:40 -0500

    Agregar 2 botones: Subir (temperatura++) y Bajar (temperatura--)

--- Semana05/tarea01/app/src/main/java/com/rau/tarea01/MainActivity.kt
+++ Semana05/tarea01/app/src/main/java/com/rau/tarea01/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
 import com.rau.tarea01.ui.theme.Tarea01Theme
 
 class MainActivity : ComponentActivity() {
@@ -88,5 +89,15 @@ fun TemperatureDisplay() {
             text = "Temperatura: $temperatura°C",
             style = MaterialTheme.typography.headlineMedium
         )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            Button(onClick = { temperatura++ }) {
+                Text("Subir")
+            }
+            Button(onClick = { temperatura-- }) {
+                Text("Bajar")
+            }
+        }
     }
 }
